69da90ca3640138264e9f898086725bc7ee6f6cd
GITmkapera <kapera123@wp.pl>
2021-11-03 18:35:09 +0100

    Update 3.11.2021

---
+++ BochenekApp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BochenekApp.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+       
+        public ShellViewModel()
+        {
+
+        }
+
+
+        public void LoadPageOne()
+        {
+            ActivateItemAsync(new FormViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItemAsync(new TableViewModel());
+        }
+    }
+}
